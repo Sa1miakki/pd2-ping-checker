c5a08f861440e021c26406a4969c875b4dd54b24
Sa1miakki <140953518+Sa1miakki@users.noreply.github.com>
2024-06-12 12:00:22 +0800

    Add files via upload

--- lua/mod.lua
+++ lua/mod.lua
@@ -1,12 +1,17 @@
-local host = managers.network:session():peer(1)
-
-function Ping_check_loop(interval)
-    standard = standard or 0
-    if host then ms1 = host:qos().ping end
-	startchecking = true
-    DelayedCalls:Add("Ping_check_loop", interval, function()
-		if host then ms2 = host:qos().ping end
+function Ping_check_loop(interval, host)
+	standard = standard or 0
+    
+	if host then 
+	    ms1 = host:qos().ping
+	end
+    
+	DelayedCalls:Add("Ping_check_loop", interval, function()
+		if host then 
+	        ms2 = host:qos().ping
+	    end
+		
 		dms = ms2 - ms1
+		
 		if dms == 0 then
 		    standard = standard + 1
 		else 
@@ -22,15 +27,25 @@ function Ping_check_loop(interval)
 			managers.chat:_receive_message(1,"Ping Checker","The connection to the server is unstable",Color.red)
         end
 		
-		if host then
-		    Ping_check_loop(2) 
+		if ms1 == 0 and ms2 == 0 then
+		    server_checked = true
+		else
+		    server_checked = false
+		end
+		
+		if host and not server_checked then
+		    Ping_check_loop(2, host) 
 		end
 	end)
 end
 
-if not startchecking and not Network:is_server() then
-   Ping_check_loop(2)
-else end
+Hooks:PostHook(NetworkPeer, "sync_lobby_data", "ping_chseck_sync_lobby_data", function(self, peer, ...)
+	local host = managers.network:session():peer(1)
+	
+	if not Network:is_server() then
+	    Ping_check_loop(2, host)
+	end
+end)
 
 
 
